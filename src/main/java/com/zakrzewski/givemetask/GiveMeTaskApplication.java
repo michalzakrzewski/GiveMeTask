13ce2ef2d996f00c49c347832b8e95b21350f03e
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2020-02-23 14:49:49 +0100

    first commit

---
+++ src/main/java/com/zakrzewski/givemetask/GiveMeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zakrzewski.givemetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveMeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveMeTaskApplication.class, args);
+	}
+
+}
