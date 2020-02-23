5c62b7b97a6ec2f543e239d21577e8b2d2be1a82
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2020-02-23 23:39:07 +0100

    {GM-01} Dodanie entity user oraz pierwszy controller z nim związany

--- src/main/java/com/zakrzewski/givemetask/GiveMeTaskApplication.java
+++ src/main/java/com/zakrzewski/givemetask/GiveMeTaskApplication.java
@@ -1,13 +1,18 @@
 package com.zakrzewski.givemetask;
 
+import com.zakrzewski.givemetask.entities.UserModel;
+import com.zakrzewski.givemetask.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class GiveMeTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiveMeTaskApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/zakrzewski/givemetask/services/UserService.java
@@ -0,0 +1,30 @@
+package com.zakrzewski.givemetask.services;
+
+import com.zakrzewski.givemetask.entities.UserModel;
+import com.zakrzewski.givemetask.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.jws.soap.SOAPBinding;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<UserModel> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public UserModel saveUser(UserModel newUser){
+        return userRepository.save(newUser);
+    }
+}
